7eec1f5c0e097768fe64f5db44156aa178cd29c7
Darin <darinp@gmail.com>
2020-03-12 16:16:20 -0700

    ptp

--- ptp_eval.go
+++ ptp_eval.go
@@ -12,6 +12,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 	"syscall"
 	"unsafe"
@@ -25,11 +26,26 @@ func main() {
 	}
 	fmt.Printf("Time per now() call %v\n", time.Now().Sub(start)/count)
 
-        start = time.Now()
-        for i := 0; i < count; i++ {
-                _ = gettime(0)
-        }
-        fmt.Printf("Time per gettime(CLOCK_REALTIME) call %v\n", time.Now().Sub(start)/count)
+	ptp_dev, err := os.Open("/dev/ptp0")
+	if err == nil {
+		ptp_fd := ptp_dev.Fd()
+		fmt.Printf("Opened /dev/ptp0 with fd %d", ptp_fd)
+
+		start = time.Now()
+		for i := 0; i < count; i++ {
+			_ = gettime(ptp_fd)
+		}
+		fmt.Printf("Time per gettime(/dev/ptp0) call %v\n", time.Now().Sub(start)/count)
+	} else {
+		fmt.Printf("Can't open /dev/ptp0: %+v", err)
+	}
+
+
+	start = time.Now()
+	for i := 0; i < count; i++ {
+		_ = gettime(0)
+	}
+	fmt.Printf("Time per gettime(CLOCK_REALTIME) call %v\n", time.Now().Sub(start)/count)
 
 	start = time.Now()
 	for i := 0; i < count; i++ {
@@ -40,7 +56,7 @@ func main() {
 	
 }
 
-func gettime(clock_id int) uint64 {
+func gettime(clock_id uintptr) uint64 {
 	var ts syscall.Timespec
 	syscall.Syscall(syscall.SYS_CLOCK_GETTIME, 1, uintptr(unsafe.Pointer(&ts)), 0)
 	return uint64(ts.Nano())
