c066a0c5310efb02d17b7fddc6829f26366eeefd
Darin <darinp@gmail.com>
2020-03-12 16:29:32 -0700

    nsec

--- ptp_eval.go
+++ ptp_eval.go
@@ -32,26 +32,35 @@ func main() {
 		fmt.Printf("Opened /dev/ptp0 with fd %d\n", ptp_fd)
 
 		start = time.Now()
+		startNSec := gettime(ptp_fd)
 		for i := 0; i < count; i++ {
 			_ = gettime(ptp_fd)
 		}
-		fmt.Printf("Time per gettime(/dev/ptp0) call %v\n", time.Now().Sub(start)/count)
+		end := time.Now()
+		endNSec := gettime(ptp_fd)
+		fmt.Printf("Time per gettime(/dev/ptp0) call %v, nsec diff: %v\n", end.Sub(start)/count, (endNSec-startNSec)/count)
 	} else {
 		fmt.Printf("Can't open /dev/ptp0: %+v\n", err)
 	}
 
 
 	start = time.Now()
+	startNSec := gettime(0)
 	for i := 0; i < count; i++ {
 		_ = gettime(0)
 	}
-	fmt.Printf("Time per gettime(CLOCK_REALTIME) call %v\n", time.Now().Sub(start)/count)
+	end := time.Now()
+	endNSec := gettime(0)
+	fmt.Printf("Time per gettime(CLOCK_REALTIME) call %v, nsec diff: %v\n", end.Sub(start)/count, (endNSec-startNSec)/count)
 
 	start = time.Now()
+	startNSec = gettime(1)
 	for i := 0; i < count; i++ {
 		_ = gettime(1)
 	}
-	fmt.Printf("Time per gettime(CLOCK_MONOTONIC) call %v\n", time.Now().Sub(start)/count)
+	end = time.Now()
+	endNSec = gettime(1)
+	fmt.Printf("Time per gettime(CLOCK_MONOTONIC) call %v, nsec diff: %v\n", end.Sub(start)/count, (endNSec-startNSec)/count)
 
 	
 }
