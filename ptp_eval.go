39b0c12e551c174be58b1355fe961733aa873566
Darin <darinp@gmail.com>
2020-03-23 15:48:16 -0700

    thread

--- ptp_eval.go
+++ ptp_eval.go
@@ -20,6 +20,7 @@ import "C"
 import (
 	"fmt"
 	"os"
+	"sync"
 	"time"
 )
 const count = 5_000_000
@@ -59,23 +60,33 @@ func main() {
 	}
 	end = time.Now()
 	fmt.Printf("Time per nanotime() call %v, nsec diff: %v\n", end.Sub(start)/count, (endNSec-startNSec)/count)
+	for tc := 0; tc < 5; tc++ {
+		fmt.Printf("*** Thread count %d\n", 1<<tc)
+		ptp_dev, err := os.Open(ptpDevice)
+		if err == nil {
+			ptp_fd := ptp_dev.Fd()
+			ptp_mod_fd := (^ptp_fd << 3) | 3
+			fmt.Printf("Opened %s with fd %x, mod_fd %x \n", ptpDevice, ptp_fd, ptp_mod_fd)
+			TryGetTimeCGO(ptp_mod_fd, fmt.Sprintf("C.clock_gettime(%s)", ptpDevice), 1<<tc)
+		} else {
+			fmt.Printf("Can't open %s: %+v\n", ptpDevice, err)
+		}
 
-	ptp_dev, err := os.Open(ptpDevice)
-	if err == nil {
-		ptp_fd := ptp_dev.Fd()
-		ptp_mod_fd := (^ptp_fd << 3) | 3
-		fmt.Printf("Opened %s with fd %x, mod_fd %x \n", ptpDevice, ptp_fd, ptp_mod_fd)
-		TryGetTimeCGO(ptp_mod_fd, fmt.Sprintf("C.clock_gettime(%s)", ptpDevice))
-	} else {
-		fmt.Printf("Can't open %s: %+v\n", ptpDevice, err)
+		TryGetTimeCGO(C.CLOCK_REALTIME, "C.clock_gettime(CLOCK_REALTIME)", 1<<tc)
+		TryGetTimeCGO(C.CLOCK_MONOTONIC, "C.clock_gettime(CLOCK_MONOTONIC)", 1<<tc)
 	}
-
-	TryGetTimeCGO(C.CLOCK_REALTIME, "C.clock_gettime(CLOCK_REALTIME)")
-	TryGetTimeCGO(C.CLOCK_MONOTONIC, "C.clock_gettime(CLOCK_MONOTONIC)")
 	fmt.Printf("now is %s\n", time.Now())
 }
 
-func TryGetTimeCGO(clockId uintptr, text string) {
+func CheckSingleThreadPerf(clockId uintptr, count int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var ts C.struct_timespec
+	for i := 0; i < count; i++ {
+		_ = C.clock_gettime(C.clockid_t(clockId), &ts)
+	}
+}
+
+func TryGetTimeCGO(clockId uintptr, text string, threadCount int) {
 	start := time.Now()
 	var ts C.struct_timespec
 	_, err := C.clock_gettime(C.clockid_t(clockId), &ts)
@@ -83,9 +94,13 @@ func TryGetTimeCGO(clockId uintptr, text string) {
 		panic(err)
 	}
 	startNSec := uint64(ts.tv_sec)*1e9 + uint64(ts.tv_nsec)
-	for i := 0; i < count; i++ {
-		_ = C.clock_gettime(C.clockid_t(clockId), &ts)
+	var wg sync.WaitGroup
+	wg.Add(threadCount)
+	for t := 0; t < threadCount; t++ {
+		go CheckSingleThreadPerf(clockId, count/threadCount, &wg)
 	}
+	wg.Wait()
+	_ = C.clock_gettime(C.clockid_t(clockId), &ts)
 	end := time.Now()
 	endNSec := uint64(ts.tv_sec)*1e9 + uint64(ts.tv_nsec)
 	fmt.Printf("CGO %s call %v, end now: %s, end get time: %s, nsec diff: %v\n",
